7312c9bdbd6ef424bf9fc8ed3c4f9171618f6197
anar <sadiglianar@gmail.com>
2023-11-27 21:42:06 +0200

    implement debouncing on input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Country } from './types/country';
 
 export default function App() {
 	const [userInput, setUserInput] = useState<string>('');
-	console.log({ userInput });
+	const [loading, setLoading] = useState<boolean>(false);
+	const [countries, setCountries] = useState<Country[]>([]);
+
+	async function fetchCountries() {
+		try {
+			setLoading(true);
+			const response = { data: [{ code: 'SP', name: 'Spain' }] }; // TODO: implement api call
+			console.log(response.data);
+			setCountries(response.data);
+		} catch (error) {
+			console.error('Error while fetching countries:', error);
+		} finally {
+			setLoading(false);
+		}
+	}
+	useEffect(() => {
+		const debounceTimer = setTimeout(() => {
+			if (userInput) {
+				fetchCountries();
+			}
+		}, 1000);
+
+		return () => clearTimeout(debounceTimer);
+	}, [userInput]);
 
 	return (
 		<div>
@@ -13,6 +37,16 @@ export default function App() {
 				value={userInput}
 				onChange={(event) => setUserInput(event.target.value)}
 			/>
+
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<ul>
+					{countries.map((country) => (
+						<li key={country.code}>{country.name}</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 }
